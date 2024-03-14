9d6c9789c28a89b78f537cf9f840454672e6febf
MATIAS ASTETE <m.astete@uc.cl>
2024-03-14 12:30:03 -0300

    memoria y dado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,3 +1,5 @@
+import random
+
 def juego_del_dado():
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
@@ -5,4 +7,19 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
-    pass
+    suma_jugador = 0
+    suma_pc = 0
+    while suma_jugador < 30 and suma_pc < 30:
+        input("presione enter para lanzar su dado: " )
+        suma_jugador += random.randint(1,6)
+        if suma_jugador < 30:
+            suma_pc += random.randint(1,6)
+        print(f"suma actual jugador: {suma_jugador}")
+        print(f"suma actual pc: {suma_pc}")
+    print("termino el juego")
+    if suma_jugador >= 30:
+        print("gana el jugador")
+    else:
+        print("perdiste, gana la computadora")
+
+    return
--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,3 +1,5 @@
+import random
+
 def memoria():
     """
     Esta función representa el juego de memoria.
@@ -5,4 +7,23 @@ def memoria():
     Luego, debes pedir al usuario que repita la secuencia.
     Se debe mostrar un mensaje si el usuario acierta o no.
     """
-    pass
+    cantidad_numeros = random.randint(1, 5)
+    lista_numeros = []
+    while cantidad_numeros > 0:
+        lista_numeros.append(random.randint(1, 100))
+        cantidad_numeros -= 1
+    print(f"los numeros son: {lista_numeros}")
+    lista_jugador = []
+    print("ahora repita la secuencia, ingresando un numero a la vez:")
+    for numero_act in range(len(lista_numeros)):
+        lista_numeros[numero_act] = str(lista_numeros[numero_act])
+        numero_jugador = input()
+        lista_jugador.append(numero_jugador)
+
+    print(f"secuencia original: {lista_numeros}")
+    print(f"secuencia jugador: {lista_jugador}")
+    if lista_numeros == lista_jugador:
+        print("ganaste")
+    else:
+        print("perdiste")
+    return
